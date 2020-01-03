1ec1094d3f0af752f8893c183074b9c119c2acc4
Rupayan Neogy <rneogy@mit.edu>
2020-01-03 03:30:57 -0500

    fix port

--- server/app.js
+++ server/app.js
@@ -335,7 +335,7 @@ io.on("connection", socket => {
   });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
   console.log(`Listening on port ${port} and looking in folder ${publicPath}`);
